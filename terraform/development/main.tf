5682abcaa0b9db793ac97871c20f2b08b71f96bf
duncan <duncanuk@gmail.com>
2022-04-04 11:00:14 +0100

    adds TA url to CORS rules

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -23,7 +23,10 @@ resource "aws_s3_bucket" "frontend-bucket-development" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
-    allowed_origins = ["https://manage-my-home-development.hackney.gov.uk"]
+    allowed_origins = [
+      "https://manage-my-home-development.hackney.gov.uk",
+      "https://temporary-accommodation-development.hackney.gov.uk"
+    ]
     expose_headers  = ["x-amz-server-side-encryption","x-amz-request-id","x-amz-id-2"]
     max_age_seconds = 3000
   }
