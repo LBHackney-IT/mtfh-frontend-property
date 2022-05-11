cc8c5e311996223887efffdc7f6ab64d14d27e99
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2022-05-11 12:17:47 +0100

    change text

--- src/views/asset-view/layout.tsx
+++ src/views/asset-view/layout.tsx
@@ -49,12 +49,7 @@ const AssetSideBar = ({ assetDetails, ...properties }: AssetSideBarProperties) =
               Abusive language
             </Heading>
             <p style={{ fontSize: "16px", lineHeight: "20px", margin: 0 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Tenant verbally abusive to operative, whilst working in tenants property.
             </p>
           </AccordionItem>
         </Accordion>
