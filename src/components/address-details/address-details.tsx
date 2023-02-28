fc0007c5104553f77d71fbbfcfc03bca60f74037
ange47rm <ange47rm@hotmail.com>
2023-02-28 11:19:18 +0000

    Implemented Formik and form schema with Yup. Form rendered correctly with no errors.

--- src/components/address-details/address-details.tsx
+++ src/components/address-details/address-details.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 import { Asset } from "@mtfh/common/lib/api/asset/v1";
+import { Field, Form, Formik, useFormikContext } from "formik";
+import { AddressDetailsFormData } from "./schema";
 
-export interface CurrentAddressProperties {
+export interface AddressDetailsProperties {
     assetDetails: Asset;
     heading: string;
     readOnly: boolean;
@@ -12,91 +14,51 @@ export const AddressDetails = ({
     assetDetails,
     heading = 'Address details',
     readOnly = false
-}: CurrentAddressProperties): JSX.Element => {
+}: AddressDetailsProperties): JSX.Element => {
     return (
         <>
-            <h3 className="lbh-heading-h3">{heading}</h3>
+            <Formik<AddressDetailsFormData>
+                initialValues={{
+                    addressLine1: assetDetails.assetAddress.addressLine1,
+                    addressLine2: assetDetails.assetAddress.addressLine2,
+                    addressLine3: assetDetails.assetAddress.addressLine3,
+                    addressLine4: assetDetails.assetAddress.addressLine4,
+                    postcode: assetDetails.assetAddress.postCode
+                }}
+                onSubmit={async (values) => {
+                    await new Promise((r) => setTimeout(r, 500));
+                    alert(JSON.stringify(values, null, 2));
+                }}
+            >
+                <Form>
+                    <h3 className="lbh-heading-h3">{heading}</h3>
 
-            <div className="govuk-form-group lbh-form-group">
-                <label className="govuk-label lbh-label" htmlFor="address-line-one">
-                    Address line 1*
-                </label>
-                <input
-                    className="govuk-input lbh-input"
-                    id="address-line-one-input"
-                    name="address-line-one"
-                    type="text"
-                    required
-                    value={assetDetails.assetAddress.addressLine1}
-                    disabled={readOnly}
-                />
-            </div>
-            <div className="govuk-form-group lbh-form-group">
-                <label className="govuk-label lbh-label" htmlFor="address-line-two">
-                    Address line 2
-                </label>
-                <input
-                    className="govuk-input lbh-input"
-                    id="address-line-two-input"
-                    name="address-line-two"
-                    type="text"
-                    value={assetDetails.assetAddress.addressLine2}
-                    disabled={readOnly}
-                />
-            </div>
-            <div className="govuk-form-group lbh-form-group">
-                <label className="govuk-label lbh-label" htmlFor="address-line-three">
-                    Address line 3
-                </label>
-                <input
-                    className="govuk-input lbh-input"
-                    id="address-line-three-input"
-                    name="address-line-three"
-                    type="text"
-                    value={assetDetails.assetAddress.addressLine3}
-                    disabled={readOnly}
-                />
-            </div>
-            <div className="govuk-form-group lbh-form-group">
-                <label className="govuk-label lbh-label" htmlFor="address-line-three">
-                    Address line 4
-                </label>
-                <input
-                    className="govuk-input lbh-input"
-                    id="address-line-three-input"
-                    name="address-line-three"
-                    type="text"
-                    value={assetDetails.assetAddress.addressLine4}
-                    disabled={readOnly}
-                />
-            </div>
-            <div className="govuk-form-group lbh-form-group">
-                <label className="govuk-label lbh-label" htmlFor="town">
-                    Town
-                </label>
-                <input
-                    className="govuk-input lbh-input"
-                    id="town-input"
-                    name="town"
-                    type="text"
-                    required
-                    disabled={readOnly}
-                />
-            </div>
-            <div className="govuk-form-group lbh-form-group">
-                <label className="govuk-label lbh-label" htmlFor="postcode">
-                    Postcode
-                </label>
-                <input
-                    className="govuk-input lbh-input"
-                    id="postcode-input"
-                    name="postcode"
-                    type="text"
-                    required
-                    value={assetDetails.assetAddress.postCode}
-                    disabled={readOnly}
-                />
-            </div>
+                    <label className="govuk-label lbh-label" htmlFor="addressLine1">
+                        Address line 1*
+                    </label>
+                    <Field id="addressLine1" name="addressLine1" className="govuk-input lbh-input" type="text" required disabled={readOnly} />
+                    
+                    <label className="govuk-label lbh-label" htmlFor="addressLine2">
+                        Address line 2
+                    </label>
+                    <Field id="addressLine2" name="addressLine2" className="govuk-input lbh-input" type="text" required disabled={readOnly} />
+
+                    <label className="govuk-label lbh-label" htmlFor="addressLine3">
+                        Address line 3
+                    </label>
+                    <Field id="addressLine3" name="addressLine3" className="govuk-input lbh-input" type="text" required disabled={readOnly} />
+
+                    <label className="govuk-label lbh-label" htmlFor="addressLine4">
+                        Address line 4
+                    </label>
+                    <Field id="addressLine4" name="addressLine4" className="govuk-input lbh-input" type="text" required disabled={readOnly} />
+
+                    <label className="govuk-label lbh-label" htmlFor="postcode">
+                        Postcode
+                    </label>
+                    <Field id="postcode" name="postcode" className="govuk-input lbh-input" type="text" required disabled={readOnly} />                    
+                </Form>
+            </Formik>
         </>
     );
 };
---
+++ src/components/address-details/schema.ts
@@ -0,0 +1,14 @@
+import * as Yup from "yup";
+
+export const addressDetailsSchema = () =>
+  Yup.object({
+    addressLine1: Yup.string().required(),
+    addressLine2: Yup.string().required(),
+    addressLine3: Yup.string().required(),
+    addressLine4: Yup.string().required(),
+    postcode: Yup.string().required(),
+  });
+
+export type AddressDetailsFormData = Yup.Asserts<
+  ReturnType<typeof addressDetailsSchema>
+>;
