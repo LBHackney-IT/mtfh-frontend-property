6baf71cc96c459bff9d1645952c2f997681a6810
ange47rm <ange47rm@hotmail.com>
2023-03-02 14:24:18 +0000

    Modified schema to be user friendly for error messages shown to the users

--- src/components/edit-asset-address-form/editable-address/schema.ts
+++ src/components/edit-asset-address-form/editable-address/schema.ts
@@ -5,15 +5,17 @@ import * as Yup from "yup";
 export const regexUkPostcode = /^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/;
 
 export const editableAddressSchema = () =>
+// The below are capitalized as they may be used in error messages (ex: "AddressLine1 is a required field")
   Yup.object({
-    editableAddressLine1: Yup.string().required(),
-    editableAddressLine2: Yup.string(),
-    editableAddressLine3: Yup.string(),
-    editableAddressLine4: Yup.string(),
-    editablePostcode: Yup.string()
+    AddressLine1: Yup.string().required(),
+    AddressLine2: Yup.string(),
+    AddressLine3: Yup.string(),
+    AddressLine4: Yup.string(),
+    Postcode: Yup.string()
     .trim()
     .transform(removeWhitespace)
-    .matches(regexUkPostcode, "Please enter a valid postcode").required(),
+    .matches(regexUkPostcode, "Please enter a valid postcode")
+    .required(),
   });
 
 export type EditableAddressFormData = Yup.Asserts<
