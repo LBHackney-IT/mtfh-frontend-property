c6ed857d3e2bb3468d12617c7f8f23a7b3db514b
ange47rm <ange47rm@hotmail.com>
2023-03-09 12:48:02 +0000

    Sonarcloud code smell fix

--- src/components/edit-asset-address-form/editable-address/editable-address.tsx
+++ src/components/edit-asset-address-form/editable-address/editable-address.tsx
@@ -28,9 +28,9 @@ export interface EditableAddressProperties {
 
 interface PatchAssetFormValues {
   addressLine1: string;
-  addressLine2?: string | undefined;
-  addressLine3?: string | undefined;
-  addressLine4?: string | undefined;
+  addressLine2?: string;
+  addressLine3?: string;
+  addressLine4?: string;
   postcode: string;
 }
 
