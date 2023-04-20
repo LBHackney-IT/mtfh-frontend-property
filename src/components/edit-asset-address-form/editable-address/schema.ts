129f9bb107a0fadbe1b0c4319ecb61943613865e
ange47rm <ange47rm@hotmail.com>
2023-04-20 16:47:25 +0100

    Edited Regex used for validating postcodes to be the same as the one used in the API/BE helper function. Created single file to store Regex.

--- src/components/edit-asset-address-form/editable-address/schema.ts
+++ src/components/edit-asset-address-form/editable-address/schema.ts
@@ -1,9 +1,7 @@
 import * as Yup from "yup";
 
 import { removeWhitespace } from "@mtfh/common/lib/utils";
-
-export const regexUkPostcode =
-  /^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/;
+import { regexUkPostcode } from "../../../utils/uk-postcode-regex";
 
 export const editableAddressSchema = () =>
   Yup.object({
---
+++ src/utils/uk-postcode-regex.ts
@@ -0,0 +1,1 @@
+export const regexUkPostcode = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/;
