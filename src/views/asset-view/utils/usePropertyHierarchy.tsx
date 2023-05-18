9520a974a3457d54e73a4b0cc50231de870f8f5c
ange47rm <ange47rm@hotmail.com>
2023-05-18 16:56:04 +0100

    Refined generateAssetHierarchyObject

--- src/views/asset-view/utils/usePropertyHierarchy.tsx
+++ src/views/asset-view/utils/usePropertyHierarchy.tsx
@@ -7,6 +7,11 @@ interface RelatedAssetsResponse {
     childrenAssets: Asset[];
 }
 
+interface PropertyHierarchyObject {
+    level: number;
+    assets: Asset[];
+}
+
 // Example API response
 // const relatedAssetResponse = {
 //     rootAsset: rootAsset,
@@ -27,10 +32,14 @@ export const usePropertyHierarchy = (relatedAssetResponse: RelatedAssetsResponse
 
 }
 
+const addAssetsToHierarchyLevel = (hierarchyLevel: number, assets: Asset[], hierarchyArray: PropertyHierarchyObject[]) => {
+    hierarchyArray.push({ level: hierarchyLevel, assets: assets })
+}
+
 
 const generateAssetHierarchyObject = (relatedAssetResponse: RelatedAssetsResponse, currentAsset: Asset) => {
 
-    let hierarchyArray = [];
+    const hierarchyArray: PropertyHierarchyObject[] = [];
     let hierarchyLevel = 0;
 
     // Given a list of related asset (API response) in this case "relatedAssetResponse"
@@ -62,21 +71,24 @@ const generateAssetHierarchyObject = (relatedAssetResponse: RelatedAssetsRespons
 
     // If we have a rootAsset, then this will always sit at the top level (level 0)
     if (relatedAssetResponse.rootAsset) {
-        hierarchyArray.push({ [hierarchyLevel]: relatedAssetResponse.rootAsset });
+        const assetsForCurrentLevel = [relatedAssetResponse.rootAsset]
+        addAssetsToHierarchyLevel(hierarchyLevel, assetsForCurrentLevel, hierarchyArray)
         hierarchyLevel++;
     }
 
     // If there is no parent asset (different from rootAsset), current asset will sit one level down from rootAsset (lvl 1)
     if (relatedAssetResponse.parentAssets.length === 0) {
-        hierarchyArray.push({ [hierarchyLevel]: currentAsset });
+        const assetsForCurrentLevel = [currentAsset]
+        addAssetsToHierarchyLevel(hierarchyLevel, assetsForCurrentLevel, hierarchyArray)
         hierarchyLevel++;
     } else if ((relatedAssetResponse.parentAssets.length === 1)) {
         // If there ONE a parent asset (that's not the rootAsset), it will always sit at lvl 1 between rootAsset (lvl 0) and the current asset (lvl 2)
-        hierarchyArray.push({ [hierarchyLevel]: relatedAssetResponse.parentAssets[0] });
+        const assetsForCurrentLevel = [relatedAssetResponse.parentAssets[0]]
+        addAssetsToHierarchyLevel(hierarchyLevel, assetsForCurrentLevel, hierarchyArray)
         hierarchyLevel++;
 
-        // And the current asset will sit below it
-        hierarchyArray.push({ [hierarchyLevel]: currentAsset });
+        // And the current asset will sit below it at lvl 2
+        addAssetsToHierarchyLevel(hierarchyLevel, [currentAsset], hierarchyArray)
         hierarchyLevel++;
     }
 
@@ -86,21 +98,30 @@ const generateAssetHierarchyObject = (relatedAssetResponse: RelatedAssetsRespons
 
     // REVIEW
     // In the same way as above, we ideally want to find the immediate child or children, so again we'd need a Asset Type hierarchy system
-    // The below, currently, just checks if there are any children asset, if yes it adds the first one to the lower level.
+    // The below, currently, just checks if there are any children asset, if yes it adds the first one to the lower/current level.
     if (relatedAssetResponse.childrenAssets.length > 0) {
-        hierarchyArray.push({ [hierarchyLevel]: relatedAssetResponse.childrenAssets[0] });
+        const assetsForCurrentLevel = [relatedAssetResponse.childrenAssets[0]]
+        addAssetsToHierarchyLevel(hierarchyLevel, assetsForCurrentLevel, hierarchyArray)
         hierarchyLevel++;
     }
 
     return hierarchyArray;
 
     // example return
-    // hierarchyArray[
-    //     { '0': { id: 'id1', addressLine1: 'address 1', assetType: 'Estate' } },
-    //     { '1': { id: 'id2', addressLine1: 'address 2', assetType: 'Block' } },
-    //     { '2': { id: 'id3', addressLine1: 'address 3', assetType: 'Dwelling' } },
-    //     { '3': { id: 'id4', addressLine1: 'address 4', assetType: 'Boiler House' } }
+    // hierarchyArray = [
+    //     {
+    //         level: 0
+	// 	       assets: [asset]
+    //     },
+    //     {
+    //         level: 1
+	// 	       assets: [asset, asset, asset]
+    //     }
     // ]
+
 }
+
+const generatePropertyHierarchyElements = ()
+
 
 export { generateAssetHierarchyObject };
