acba7240c970bc5e6c41d43c5ed278002d6c81b1
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-06-21 11:30:32 +0100

    Comment out new asset view

--- src/views/new-asset-view/new-asset-view.tsx
+++ src/views/new-asset-view/new-asset-view.tsx
@@ -2,21 +2,23 @@ import React from "react";
 
 import { locale } from "../../services";
 import { assetAdminAuthGroups } from "../../services/config/config";
-import { NewPropertyLayout } from "./layout";
+// import { NewPropertyLayout } from "./layout";
 
-import { isAuthorisedForGroups } from "@mtfh/common/lib/auth";
+// import { isAuthorisedForGroups } from "@mtfh/common/lib/auth";
 import { ErrorSummary } from "@mtfh/common/lib/components";
 
 export const NewAssetView = (): JSX.Element => {
-  if (!isAuthorisedForGroups(assetAdminAuthGroups)) {
-    return (
-      <ErrorSummary id="unauthorized-error" title={locale.errors.noNewAssetPermissions} />
-    );
-  }
+  <ErrorSummary id="unauthorized-error" title={locale.errors.noNewAssetPermissions} />
+  
+  // if (!isAuthorisedForGroups(assetAdminAuthGroups)) {
+  //   return (
+  //     <ErrorSummary id="unauthorized-error" title={locale.errors.noNewAssetPermissions} />
+  //   );
+  // }
 
-  return (
-    <>
-      <NewPropertyLayout />
-    </>
-  );
+  // return (
+  //   <>
+  //     <NewPropertyLayout />
+  //   </>
+  // );
 };
