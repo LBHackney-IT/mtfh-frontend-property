d84a39d54322765ffaaee3c86f6d3299539a0513
ange47rm <ange47rm@hotmail.com>
2023-06-23 14:24:45 +0100

    Re-enabled New Asset feature

--- src/views/new-asset-view/new-asset-view.tsx
+++ src/views/new-asset-view/new-asset-view.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 
 import { locale } from "../../services";
-// import { assetAdminAuthGroups } from "../../services/config/config";
-// import { NewPropertyLayout } from "./layout";
+import { assetAdminAuthGroups } from "../../services/config/config";
+import { NewPropertyLayout } from "./layout";
 
-// import { isAuthorisedForGroups } from "@mtfh/common/lib/auth";
+import { isAuthorisedForGroups } from "@mtfh/common/lib/auth";
 import { ErrorSummary } from "@mtfh/common/lib/components";
 
 export const NewAssetView = (): JSX.Element => {
+  if (!isAuthorisedForGroups(assetAdminAuthGroups)) {
+    return (
+      <ErrorSummary id="unauthorized-error" title={locale.errors.noNewAssetPermissions} />
+    );
+  }
+
   return (
-    <ErrorSummary id="unauthorized-error" title={locale.errors.noNewAssetPermissions} />
+    <>
+      <NewPropertyLayout />
+    </>
   );
-
-  // if (!isAuthorisedForGroups(assetAdminAuthGroups)) {
-  //   return (
-  //     <ErrorSummary id="unauthorized-error" title={locale.errors.noNewAssetPermissions} />
-  //   );
-  // }
-
-  // return (
-  //   <>
-  //     <NewPropertyLayout />
-  //   </>
-  // );
 };
