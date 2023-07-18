973ed42bb30bf85e52918528aa17b5577ecf63d4
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-07-18 10:18:13 +0100

    Remove useConfirmationRemoveModal hook

--- src/components/boiler-house-details/boiler-house-details.tsx
+++ src/components/boiler-house-details/boiler-house-details.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 import { locale } from "../../services";
 import { ConfirmationModal } from "./confirmation-modal";
 import { useBoilerHouseDetails } from "./hooks/useBoilerHouseDetails";
-import { useConfirmRemoveModal } from "./hooks/useConfirmRemoveModal";
 
 import { Asset } from "@mtfh/common/lib/api/asset/v1";
 import { Button, Center, Heading, Link, Spinner } from "@mtfh/common/lib/components";
@@ -17,20 +16,14 @@ export const BoilerHouseDetails = ({ asset }: Props) => {
   const { isLoading, boilerHouseAsset, assetHasBoilerHouse } =
     useBoilerHouseDetails(asset);
 
-  const {
-    isLoading: isLoadingRemoveAssetRequest,
-    setShowModal,
-    showModal,
-    handleRemoveBoilerHouse,
-  } = useConfirmRemoveModal(asset);
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <aside className="mtfh-cautionary-alerts">
       <ConfirmationModal
+        asset={asset}
         showModal={showModal}
-        hideModal={() => setShowModal(false)}
-        onSubmit={handleRemoveBoilerHouse}
-        isLoading={isLoadingRemoveAssetRequest}
+        setShowModal={setShowModal}
       />
 
       <Heading variant="h2" className="lbh-heading lbh-heading-h3">
--- src/components/boiler-house-details/confirmation-modal.tsx
+++ src/components/boiler-house-details/confirmation-modal.tsx
@@ -1,37 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { locale } from "../../services";
 
 import { Button, Dialog, DialogActions, Link } from "@mtfh/common/lib/components";
+import { Asset, PatchAssetRequest, patchAsset } from "@mtfh/common/lib/api/asset/v1";
 
 interface Props {
+  asset: Asset;
   showModal: boolean;
-  hideModal: () => void;
-  onSubmit: () => void;
-  isLoading: boolean;
+  setShowModal: (value: React.SetStateAction<boolean>) => void;
 }
 
-export const ConfirmationModal = ({
-  showModal,
-  hideModal,
-  onSubmit,
-  isLoading,
-}: Props) => {
+export const ConfirmationModal = ({ showModal, setShowModal, asset }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = () => {
+    if (isLoading) return;
+
+    const request: PatchAssetRequest = {
+      boilerHouseId: "",
+    };
+
+    setIsLoading(true);
+
+    patchAsset(asset.id, request, asset?.versionNumber?.toString() || "")
+      .then(() => {
+        setShowModal(false);
+        // useAsset doesn't always update
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   return (
     <Dialog
       isOpen={showModal}
-      onDismiss={hideModal}
+      onDismiss={() => setShowModal(false)}
       title={locale.boilerHouseDetails.modal.heading}
     >
       <DialogActions>
         <Button
-          onClick={onSubmit}
+          onClick={handleSubmit}
           isLoading={isLoading}
           data-testid="confirm-remove-modal-button"
         >
           Remove
         </Button>
-        <Link as="button" type="button" onClick={hideModal}>
+        <Link as="button" type="button" onClick={() => setShowModal(false)}>
           Cancel
         </Link>
       </DialogActions>
