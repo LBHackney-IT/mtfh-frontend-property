6f95b6992db384d449070c5d7841d973f0401063
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-07-20 07:48:58 +0100

    Add auth to boilerHouse view

--- src/views/add-boiler-house-view/add-boiler-house-view.tsx
+++ src/views/add-boiler-house-view/add-boiler-house-view.tsx
@@ -6,11 +6,21 @@ import { AddBoilerHouseLayout } from "./layout";
 
 import { useAsset } from "@mtfh/common/lib/api/asset/v1";
 import { Center, ErrorSummary, Spinner } from "@mtfh/common/lib/components";
+import { assetAdminAuthGroups } from "services/config/config";
 
 export const AddBoilerHouseView = (): JSX.Element => {
   const { assetId } = useParams<{ assetId: string }>();
 
   const { data: asset, ...assetRequest } = useAsset(assetId);
+
+  if (!isAuthorisedForGroups(assetAdminAuthGroups)) {
+    return (
+      <ErrorSummary
+        id="unauthorized-error"
+        title={locale.errors.noAddressEditPermissions}
+      />
+    );
+  }
 
   if (assetRequest.error) {
     return (
