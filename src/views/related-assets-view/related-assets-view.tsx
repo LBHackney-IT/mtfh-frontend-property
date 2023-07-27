d4fc321da109e85555dd29fbf120501205d2502d
ange47rm <ange47rm@hotmail.com>
2023-07-27 10:44:20 +0100

    RelatedAssetsView now fetches all data required to obtain related assets

--- src/views/related-assets-view/related-assets-view.tsx
+++ src/views/related-assets-view/related-assets-view.tsx
@@ -1,8 +1,33 @@
+import { useAsset, useChildAssets } from "@mtfh/common/lib/api/asset/v1";
+import { Center, ErrorSummary, Spinner } from "@mtfh/common/lib/components";
 import React from "react";
-
+import { locale } from "../../services";
+import { useParams } from "react-router-dom";
 import { RelatedAssetsLayout } from "./layout";
 
 export const RelatedAssetsView = (): JSX.Element => {
+  const { assetId } = useParams<{ assetId: string }>();
+
+  const { data: asset, ...assetRequest } = useAsset(assetId);
+  const { data: childAssetResponse } = useChildAssets(assetId);
+
+  if (assetRequest.error) {
+    return (
+      <ErrorSummary
+        id="property-error"
+        title={locale.errors.unableToFetchRecord}
+        description={locale.errors.tryAgainOrContactSupport}
+      />
+    );
+  }
+
+  if (!asset) {
+    return (
+      <Center>
+        <Spinner />
+      </Center>
+    );
+  }
 
   return (
     <>
