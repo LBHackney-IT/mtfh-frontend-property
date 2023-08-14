db138f35a4fb8c6a49e069d7fae06dec86809905
Dave Dean <david.dean@hackney.gov.uk>
2023-08-14 17:52:58 +0100

    Impl

--- src/components/new-asset-form/schema.ts
+++ src/components/new-asset-form/schema.ts
@@ -42,6 +42,8 @@ export const newPropertySchema = () =>
         }),
       )
       .nullable(),
+    
+    addDefaultSorContracts: Yup.string().required("Please select an option"),
 
     // Asset details
     numberOfBedrooms: Yup.number()
