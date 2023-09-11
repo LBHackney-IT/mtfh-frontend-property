1d04ee44e55d2fdb3f3207a3cbf9e4fafcdd0a7d
Angelo Di Massimo <ange47rm@hotmail.com>
2023-09-11 15:02:29 +0100

    Added remaining Jest/RTL tests and updated snapshot

--- src/components/edit-asset-ownership-form/edit-asset-ownership-form.test.tsx
+++ src/components/edit-asset-ownership-form/edit-asset-ownership-form.test.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import { render, server } from "@hackney/mtfh-test-utils";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import { rest } from "msw";
 
 import { EditAssetOwnershipForm } from "./edit-asset-ownership-form";
 
 import { Asset } from "@mtfh/common/lib/api/asset/v1";
+import userEvent from "@testing-library/user-event";
 
 const testAsset: Asset = {
   id: "769894bd-b0bc-47eb-a780-322372c2448f",
@@ -72,9 +73,11 @@ beforeEach(() => {
   );
 });
 
-// const setShowSuccessMock = jest.fn();
-
-// const setRequestErrorMock = () => {};
+// Form HTML elements
+const submitButton = screen.getByRole("button", { name: /confirm change/i });
+const radioOptionYes = screen.getByRole("radio", { name: /yes/i });
+const radioOptionNo = screen.getByRole("radio", { name: /no/i });
+const radioOptions = screen.getAllByRole("radio");
 
 test("it renders the component", async () => {
   const { container } = render(
@@ -97,10 +100,6 @@ test("it displays two radio buttons to set isCouncilProperty true or false", asy
     />,
   );
 
-  const radioOptions = screen.getAllByRole("radio");
-  const radioOptionYes = screen.getByRole("radio", { name: /yes/i });
-  const radioOptionNo = screen.getByRole("radio", { name: /no/i });
-
   expect(radioOptions).toHaveLength(2);
   expect(radioOptionYes).toBeVisible();
   expect(radioOptionNo).toBeVisible();
@@ -117,7 +116,50 @@ test("the radio option is already checked on page load if isCouncilProperty has 
     />,
   );
 
-  const radioOptionYes = screen.getByRole("radio", { name: /yes/i });
-
   expect(radioOptionYes).toBeChecked();
+});
+
+test("the submit button of the form starts out as disabled, and if the ownership of the asset is changed, the button is enabled", async () => {
+  render(
+    <EditAssetOwnershipForm
+      asset={testAsset}
+      setShowSuccess={jest.fn()}
+      setRequestError={jest.fn()}
+    />,
+  );
+
+  // Button is disabled to start with
+  expect(submitButton).toBeDisabled();
+
+  // Change ownership 
+  userEvent.click(radioOptionNo);
+
+  // Button is now enabled
+  expect(submitButton).toBeEnabled();
+});
+
+test("once the form has been submitted, both radio options and the submit button are disabled", async () => {
+  render(
+    <EditAssetOwnershipForm
+      asset={testAsset}
+      setShowSuccess={jest.fn()}
+      setRequestError={jest.fn()}
+    />,
+  );
+
+  // Change ownership 
+  userEvent.click(radioOptionNo);
+
+  await waitFor(async () => {
+    // Submit form
+    userEvent.click(submitButton)
+
+    // Delay to allow re-render
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    // Submit button and the radio options are disabled
+    expect(submitButton).toBeDisabled();
+    expect(radioOptionYes).toBeDisabled();
+    expect(radioOptionNo).toBeDisabled();
+  });
 });
