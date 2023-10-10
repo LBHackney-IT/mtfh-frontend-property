090793018981915dde3e2c7a1f14de3d3b7576a0
Adam <adam.tracy20@gmail.com>
2023-10-10 16:55:16 +0100

    Reassignment WIP

--- src/components/patch-assignment-form/patch-assignment-form.tsx
+++ src/components/patch-assignment-form/patch-assignment-form.tsx
@@ -1,19 +1,71 @@
 import React, { useEffect, useState } from "react";
 
-import { Patch, getAllPatchesAndAreas, ResponsibleEntity } from "@mtfh/common/lib/api/patch/v1";
-import { Spinner, Table, Tbody, Td, Th, Thead, Tr } from "@mtfh/common/lib/components";
+import {
+  Patch,
+  getAllPatchesAndAreas,
+} from "@mtfh/common/lib/api/patch/v1";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  Link,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@mtfh/common/lib/components";
+import { config } from "@mtfh/common/lib/config";
+import { axiosInstance } from "@mtfh/common/lib/http";
 
+enum ResponsibleType {
+  HousingOfficer = "HousingOfficer",
+  HousingAreaManager = "HousingAreaManager",
+}
+
+interface UpdatePatchesAndAreasRequest {
+  id: string;
+  name: string;
+  responsibleType: ResponsibleType;
+}
+
+//TODO: Move to common MFE
+//TODO: Call when confirmation button in dialog pressed
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
+const updatePatchesAndAreasResponsibilities = async (
+  patchId: string,
+  responsibleEntityId: string,
+  request: UpdatePatchesAndAreasRequest,
+): Promise<Array<Patch>> => {
+  return new Promise<Array<Patch>>((resolve, reject) => {
+    axiosInstance
+      .patch<Array<Patch>>(
+        `${config.patchesAndAreasApiUrlV1}/patch/${patchId}/responsibleEntity/${responsibleEntityId}`,
+        {
+          headers: {
+            "skip-x-correlation-id": true,
+          },
+          data: request,
+        },
+      )
+      .then((res) => resolve(res.data))
+      .catch((error) => reject(error));
+  });
+};
 
 interface Props {
   setShowSuccess: React.Dispatch<React.SetStateAction<boolean>>;
   setRequestError: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
-
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const PatchAssignmentForm = ({ setShowSuccess, setRequestError }: Props) => {
   const [patchesAndAreas, setPatchesAndAreas] = useState<Patch[]>([]);
-
   const [patchOption, setPatchOption] = useState<string>("all");
+  const [reassigningPatch, setReassigningPatch] = useState<Patch | null>(null);
+  const [dialogActive, setDialogActive] = useState(false);
 
   useEffect(() => {
     getAllPatchesAndAreas().then((data) => {
@@ -21,7 +73,24 @@ export const PatchAssignmentForm = ({ setShowSuccess, setRequestError }: Props) 
     });
   }, []);
 
-  const areas = patchesAndAreas.filter((patchOrArea) => patchOrArea.patchType === "area");
+  const areas = patchesAndAreas
+    .filter((patchOrArea) => patchOrArea.patchType === "area")
+    .sort((a, b) => (a.name > b.name ? 1 : -1));
+
+  const CancelReassignmentButton = (): JSX.Element => {
+    return (
+      <button
+        className="govuk-button lbh-button"
+        style={{ marginTop: 0 }}
+        onClick={(e) => {
+          e.preventDefault();
+          setReassigningPatch(null);
+        }}
+      >
+        Cancel
+      </button>
+    );
+  };
 
   const PatchTableHeader = (): JSX.Element => {
     return (
@@ -30,7 +99,7 @@ export const PatchAssignmentForm = ({ setShowSuccess, setRequestError }: Props) 
           <Th>Patch</Th>
           <Th>Area</Th>
           <Th>Assigned Officer</Th>
-          <Th></Th>
+          <Th />
         </Tr>
       </Thead>
     );
@@ -40,7 +109,6 @@ export const PatchAssignmentForm = ({ setShowSuccess, setRequestError }: Props) 
     patchesAndAreas,
   }: {
     patchesAndAreas: Patch[];
-
   }): JSX.Element => {
     let patches = patchesAndAreas.filter(
       (patchOrArea) => patchOrArea.patchType === "patch" && patchOrArea.name !== "E2E",
@@ -49,80 +117,107 @@ export const PatchAssignmentForm = ({ setShowSuccess, setRequestError }: Props) 
       (patchOrArea) => patchOrArea.patchType === "area" && patchOrArea.name !== "E2E",
     );
 
-    const housingOfficers: ResponsibleEntity[] = patchesAndAreas
-    .filter((patch) => patch.patchType === "patch")
-    .map(patch => patch.responsibleEntities.filter(officer => officer.responsibleType == "HousingOfficer")[0])
-
-    const officerNames: string[] = housingOfficers.map((officer) => officer.name)
-    
-
     if (areas.length === 0) {
-      return (
-        <Spinner/>
-      );
+      return <Spinner />;
     }
 
     interface PatchTableItem extends Patch {
-      parentAreaName: string | undefined
+      parentAreaName: string | undefined;
     }
 
-    var patchTableItems: PatchTableItem[] = []; 
-    if(patchOption == "all")
-    {
-      console.log(`patchesandAreas: ${patchesAndAreas}`)
-      patchTableItems = (patchesAndAreas as PatchTableItem[])
-    }
-    else 
-    {
-      var selectedArea = areas.find((area) => area.id === patchOption);
-  
-      patches = patches.filter(
-        (patch) => patch.parentId === selectedArea?.id);
-      console.log(`Patches 93: ${patches}`);
-      console.log(`selectedArea: ${selectedArea}`)
-      patchTableItems.push((selectedArea as PatchTableItem))
+    let patchTableItems: PatchTableItem[] = [];
+    if (patchOption === "all") {
+      patchTableItems = patchesAndAreas as PatchTableItem[];
+    } else {
+      const selectedArea = areas.find((area) => area.id === patchOption);
+
+      patches = patches.filter((patch) => patch.parentId === selectedArea?.id);
+      patchTableItems.push(selectedArea as PatchTableItem);
       patches.forEach((patch) => {
-        patchTableItems.push((patch as PatchTableItem))
-      })
+        patchTableItems.push(patch as PatchTableItem);
+      });
     }
-    
-    patches = patches.sort((a, b) => a.name > b.name ? 1 : -1)
-   
+
     patches.forEach((patch) => {
-      let patchListItem = (patch as PatchTableItem);
-      let parentArea = areas.filter(area => area.id == patch.parentId)[0];
+      const patchListItem = patch as PatchTableItem;
+      const parentArea = areas.filter((area) => area.id === patch.parentId)[0];
       patchListItem.parentAreaName = parentArea.name;
-      console.log(`patchListItem: ${patchListItem}`)
     });
-    patchTableItems
-    // .sort((a, b) => {return a.name > b.name ? 1:-1})
-    .sort((a,b) => {return a.patchType !== "area" && b.patchType === "area" ? 1: -1});
-    
-    console.log(patchTableItems)
-    // 
+
+    patchTableItems = patchTableItems.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+    patchTableItems.sort((a, b) => {
+      return a.patchType !== "area" && b.patchType === "area" ? 1 : -1;
+    });
+
+    const ReassignButton = ({
+      areaOrPatch,
+    }: {
+      areaOrPatch: PatchTableItem | Patch;
+    }): JSX.Element => {
+      return (
+        <button
+          className="govuk-button lbh-button"
+          style={{ marginTop: 0, width: "10em" }}
+          onClick={(e) => {
+            e.preventDefault();
+            setPatchOption("all");
+            setReassigningPatch(areaOrPatch as Patch);
+          }}
+        >
+          Reassign
+        </button>
+      );
+    };
+
+    const AssignButton = ({
+      areaOrPatch,
+    }: {
+      areaOrPatch: PatchTableItem | Patch;
+    }): JSX.Element => {
+      const reassigningOfficer = reassigningPatch?.responsibleEntities[0];
+      const officerFirstName = reassigningOfficer?.name.split(" ")[0];
+      return (
+        <button
+          className="govuk-button lbh-button"
+          style={{ marginTop: 0, width: "10em" }}
+          onClick={(e) => {
+            e.preventDefault();
+            setPatchOption("all");
+            setReassigningPatch(areaOrPatch);
+            setDialogActive(true);
+          }}
+        >
+          Assign {officerFirstName}
+        </button>
+      );
+    };
+
+    const DisplayedButton = ({patchBeingReassigned: patchBeingReassigned}: {patchBeingReassigned: Patch | null}, {patch: patch}: {patch: Patch}) => {
+      //TODO: Fix
+      if (!reassigningPatch) {
+        return <ReassignButton areaOrPatch={reassigningPatch} />;
+      }
+      const reassigningThisEntity =
+        patch.responsibleEntities[0]?.name === patchBeingReassigned.name;
+      if (reassigningThisEntity) {
+        return <AssignButton areaOrPatch={patchBeingReassigned} />;
+      }
+      return <ReassignButton areaOrPatch={patchBeingReassigned} />;
+    };
 
     return (
       <Tbody>
-        {patchTableItems
-          .map((areaOrPatch) => {
+        {patchTableItems.map((areaOrPatch) => {
           return (
             <>
               <Tr>
                 <Td>{areaOrPatch.name}</Td>
                 <Td>{areaOrPatch.parentAreaName}</Td>
-                <Td>{
-                  areaOrPatch.responsibleEntities[0]?.name}
-                </Td>
+                <Td>{areaOrPatch.responsibleEntities[0]?.name}</Td>
                 <Td>
-                <button
-                  className="govuk-button lbh-button"
-                  type="submit"
-                  data-testid="submit-button"
-                >
-                  Reassign
-                </button>
+                  <DisplayedButton patchBeingReassigned={reassigningPatch} patch={areaOrPatch} />
                 </Td>
-
               </Tr>
             </>
           );
@@ -130,10 +225,62 @@ export const PatchAssignmentForm = ({ setShowSuccess, setRequestError }: Props) 
       </Tbody>
     );
   };
+
+  const ConfirmReassignmentDialog = (): JSX.Element => {
+    return (
+      <Dialog
+        isOpen={dialogActive}
+        onDismiss={() => {
+          setDialogActive(false);
+        }}
+        title="Switch assignment"
+      >
+        <p>Reassigning officer A to patch B'</p>
+        <p>Reassigning officer B to patch A'</p>
+
+        <DialogActions>
+          <Button
+            onClick={() => {
+              console.log("hello");
+            }}
+          >
+            Confirm
+          </Button>
+
+          <Link
+            as="button"
+            onClick={() => {
+              setDialogActive(false);
+            }}
+          >
+            Cancel
+          </Link>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
+  const ReassignmentOfficerOptions = ({ patch }: { patch: Patch }): JSX.Element => {
+    const officer = patch.responsibleEntities[0];
+    return (
+      <>
+        <h2>
+          Reassigning {officer.name} from patch {patch.name}
+        </h2>
+
+        <div>
+          <CancelReassignmentButton />
+        </div>
+      </>
+    );
+  };
   return (
     <div>
+      <ConfirmReassignmentDialog />
       <form onSubmit={() => {}}>
         <div className="govuk-form-group">
+          {reassigningPatch && <ReassignmentOfficerOptions patch={reassigningPatch} />}
+
           <label className="govuk-label lbh-label" htmlFor="searchQuery">
             Area
           </label>
@@ -146,15 +293,16 @@ export const PatchAssignmentForm = ({ setShowSuccess, setRequestError }: Props) 
             style={{ marginTop: 0 }}
             data-testid="select"
           >
-            <option key={"all"} value={"all"} data-testid="select-option">
+            <option key="all" value="all" data-testid="select-option">
               All
             </option>
-            {areas?.sort((a, b) => a.name > b.name ? 1 : -1)
-            .map((area) => (
-              <option key={area.id} value={area.id} data-testid="select-option">
-                {area.name}
-              </option>
-            ))}
+            {areas
+              ?.sort((a, b) => (a.name > b.name ? 1 : -1))
+              .map((area) => (
+                <option key={area.id} value={area.id} data-testid="select-option">
+                  {area.name}
+                </option>
+              ))}
           </select>
 
           <Table>
