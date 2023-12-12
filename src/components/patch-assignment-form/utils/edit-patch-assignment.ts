fde74a7cc7f9a71567d45e3e3ea6911f1bd00e99
Adam <adam.tracy20@gmail.com>
2023-12-12 14:34:55 +0000

    Better patch error

--- src/components/patch-assignment-form/utils/edit-patch-assignment.ts
+++ src/components/patch-assignment-form/utils/edit-patch-assignment.ts
@@ -24,7 +24,14 @@ export function editPatchAssignment(
   replacePatchResponsibleEntities(patch.id, [newResEnt], currentVersionNumber)
     .then((data) => {
       if (!data) throw new Error("No data returned from API");
-      if (data.status !== 204) throw new Error(`API returned status ${data.status}`);
+      if (data.status === 409)
+        throw new Error(
+          "Patch/Area assignment has been updated by another user, please refresh the page and try again",
+        );
+      if (data.status !== 204)
+        throw new Error(
+          "Request could not be completed successfully. If this problem persists please contact support",
+        );
       patch.responsibleEntities[0] = newResEnt;
       patch.versionNumber = currentVersionNumber + 1;
       handleSubmission(true, patch);
